8c3c6421d8e5748ea587412a19acf5b93c48cafb
AlanChiangg <alaln35alan@gmail.com>
2023-06-25 18:15:30 +0800

    feat: add usersSeeder

---
+++ models/seeds/usersSeeder.js
@@ -0,0 +1,38 @@
+const db = require('../../config/mongoose')
+const USERS = require('../USERS')
+
+db.once('open', () => {
+  const users = [
+    {
+      firstName: 'Tony',
+      email: 'tony@stark.com',
+      password: 'iamironman'
+    },
+    {
+      firstName: 'Steve',
+      email: 'captain@hotmail.com',
+      password: 'icandothisallday'
+    },
+    {
+      firstName: 'Peter',
+      email: 'peter@parker.com',
+      password: 'enajyram'
+    },
+    {
+      firstName: 'Natasha',
+      email: 'natasha@gamil.com',
+      password: '*parol#@$!'
+    },
+    {
+      firstName: 'Nick',
+      email: 'nick@shield.com',
+      password: 'password'
+    }
+  ]
+
+  USERS.create(users)
+    .then(() => {
+      console.log('done!')
+      db.close()
+    })
+})
