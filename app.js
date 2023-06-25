dde540b2c57438df791e6d28a529a77b303bfcc9
AlanChiangg <alaln35alan@gmail.com>
2023-06-25 17:46:41 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Express is running on localhost:${port}`)
+})
